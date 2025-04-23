51d1d3aa1290f7137bfc8929149fc7afe7f361bb
daniel <daniel.gil.castillo@gmail.com>
2025-04-23 18:44:31 +0200

    comentario daniel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,12 +59,4 @@ dependencies {
     implementation("androidx.room:room-runtime:$roomVersion")
     kapt("androidx.room:room-compiler:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
 }
